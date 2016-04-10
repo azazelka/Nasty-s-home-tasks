34a0db7248555a8ece47ff9991dce76593ef43a2
Anastasia Tolochkova <as.tolochkova@gmail.com>
2016-04-10 15:53:20 +0300

    Tests have been refined

--- intersection/tests_for_intersection.py
+++ intersection/tests_for_intersection.py
@@ -2,27 +2,43 @@ import unittest
 
 import intersection
 
+
 class TestIntersection(unittest.TestCase):
     def test_no_intersections(self):
         array1 = [1, 2, 4, 5]
         array2 = [3, 3, 3, 3, 3]
         self.assertEqual(intersection.intersection(array1, array2), [])
+
     def test_intersection_extreme_elements(self):
-        array1 = [1,9]
+        array1 = [1, 9]
         array2 = [1, 3, 3, 3, 3, 9]
         self.assertEqual(intersection.intersection(array1, array2), [1, 9])
+
     def test_intersection_full(self):
         array1 = [3, 3, 3, 3, 7, 7, 9, 9, 12, 12]
         array2 = [3, 3, 3, 3, 7, 7, 9, 9, 12, 12]
         self.assertEqual(intersection.intersection(array1, array2), [3, 3, 3, 3, 7, 7, 9, 9, 12, 12])
+
     def test_intersection_partial(self):
         array1 = [1, 3, 3, 3, 3, 9]
         array2 = [1, 10]
         self.assertEqual(intersection.intersection(array1, array2), [1])
+
     def test_intersection_empty_array(self):
         array1 = [1]
         array2 = []
         self.assertEqual(intersection.intersection(array1, array2), [])
+
+    def test_intersection_no_iterable(self):
+        array1 = 1
+        array2 = 1
+        self.assertRaises(TypeError, intersection, array1, array2)
+
+    def test_intersection_type_error(self):
+        array1 = {1: 1, 2: 2}
+        array2 = {1: 1, 2: 2}
+        self.assertRaises(TypeError, intersection, array1, array2)
+
 
 if __name__ == '__main__':
     unittest.main()
