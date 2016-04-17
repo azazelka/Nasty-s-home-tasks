36a321399fbca0315faedaca36b59bd757159b34
Anastasia Tolochkova <as.tolochkova@gmail.com>
2016-04-17 12:41:33 +0300

    Left intersection added

--- intersection/intersection.py
+++ intersection/intersection.py
@@ -1,18 +1,3 @@
-def left_bin_search(array, item):
-    start = -1
-    end = len(array)
-    if item < array[0] or item > array[end - 1]:
-        return None
-    while start < end - 1:
-        middle = (start + end) / 2
-        if item > array[middle]:
-            start = middle
-        else:
-            end = middle
-    if end < len(array) and array[end] == item:
-        return end
-
-
 def right_bin_search(array, item, start):
     if not start:
         start = -1
@@ -29,7 +14,23 @@ def right_bin_search(array, item, start):
         return start
 
 
-def intersection(array1, array2):
+def left_bin_search(array, item, start):
+    if not start:
+        start = -1
+    end = len(array)
+    if item < array[0] or item > array[end - 1]:
+        return None
+    while start < end - 1:
+        middle = (start + end) / 2
+        if item > array[middle]:
+            start = middle
+        else:
+            end = middle
+    if end < len(array) and array[end] == item:
+        return end
+
+
+def right_intersection(array1, array2):
     if len(array1) > len(array2):
         tmp = array1
         array1 = array2
@@ -49,3 +50,22 @@ def intersection(array1, array2):
                 found_index = None
         pre_item = item
 
+
+def left_intersection(array1, array2):
+    if len(array1) > len(array2):
+        tmp = array1
+        array1 = array2
+        array2 = tmp
+    pre_item, found_index, last_found_index = None, None, None
+    for item in array1:
+        if pre_item != item:
+            found_index = left_bin_search(array2, item, last_found_index)
+            last_found_index = found_index
+            if found_index >= 0:
+                yield item
+        elif found_index < len(array2):
+            if array2[found_index + 1] == item:
+                yield item
+            else:
+                found_index = None
+        pre_item = item
--- intersection/tests_for_intersection.py
+++ intersection/tests_for_intersection.py
@@ -3,43 +3,71 @@ import unittest
 import intersection
 
 
-class TestIntersection(unittest.TestCase):
+class TestRightIntersection(unittest.TestCase):
     def test_no_intersections(self):
         array1 = [1, 2, 4, 5]
         array2 = [3, 3, 3, 3, 3]
 
-        self.assertEqual(list(intersection.intersection(array1, array2)), [])
+        self.assertEqual(list(intersection.right_intersection(array1, array2)), [])
 
     def test_intersection_extreme_elements(self):
         array1 = [1, 9]
         array2 = [1, 3, 3, 3, 3, 9]
-        self.assertEqual(list(intersection.intersection(array1, array2)), [1, 9])
+        self.assertEqual(list(intersection.right_intersection(array1, array2)), [1, 9])
 
     def test_intersection_full(self):
         array1 = [3, 3, 3, 3, 7, 7, 9, 9, 12, 12]
         array2 = [3, 3, 3, 3, 7, 7, 9, 9, 12, 12]
-        self.assertEqual(list(intersection.intersection(array1, array2)), array1)
+        self.assertEqual(list(intersection.right_intersection(array1, array2)), array1)
 
     def test_intersection_partial(self):
         array1 = [1, 3, 3, 3, 3, 9]
         array2 = [1, 10]
-        self.assertEqual(list(intersection.intersection(array1, array2)), [1])
+        self.assertEqual(list(intersection.right_intersection(array1, array2)), [1])
 
     def test_intersection_empty_array(self):
         array1 = [1]
         array2 = []
-        self.assertEqual(list(intersection.intersection(array1, array2)), [])
+        self.assertEqual(list(intersection.right_intersection(array1, array2)), [])
 
     def test_intersection_no_iterable(self):
         array1 = 1
         array2 = 1
-        self.assertRaises(TypeError, intersection, array1, array2)
+        self.assertRaises(TypeError, intersection.right_intersection, array1, array2)
 
     def test_intersection_type_error(self):
         array1 = {1: 1, 2: 2}
         array2 = {1: 1, 2: 2}
 
-        self.assertRaises(TypeError, intersection, array1, array2)
+        self.assertRaises(TypeError, intersection.right_intersection, array1, array2)
+
+
+class TestRightIntersection(unittest.TestCase):
+    def test_no_intersections(self):
+        array1 = [1, 2, 4, 5]
+        array2 = [3, 3, 3, 3, 3]
+
+        self.assertEqual(list(intersection.left_intersection(array1, array2)), [])
+
+    def test_intersection_extreme_elements(self):
+        array1 = [1, 9]
+        array2 = [1, 3, 3, 3, 3, 9]
+        self.assertEqual(list(intersection.left_intersection(array1, array2)), [1, 9])
+
+    def test_intersection_full(self):
+        array1 = [3, 3, 3, 3, 7, 7, 9, 9, 12, 12]
+        array2 = [3, 3, 3, 3, 7, 7, 9, 9, 12, 12]
+        self.assertEqual(list(intersection.left_intersection(array1, array2)), array1)
+
+    def test_intersection_partial(self):
+        array1 = [1, 3, 3, 3, 3, 9]
+        array2 = [1, 10]
+        self.assertEqual(list(intersection.left_intersection(array1, array2)), [1])
+
+    def test_intersection_empty_array(self):
+        array1 = [1]
+        array2 = []
+        self.assertEqual(list(intersection.left_intersection(array1, array2)), [])
 
 
 if __name__ == '__main__':
